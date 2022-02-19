414fa0cdc30676e3abcf30389e62557fca8bc15e
letsbehuman <letsbehumandesign@gmail.com>
2022-02-19 19:30:23 +0100

    actualizacion

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,34 +1,39 @@
-// import React, {Component} from "react";
-// import './scroll.css'
+import React, {Component} from "react";
+import './scroll.css'
 
-// class Scroll extends Component{
+class Scroll extends Component{
+constructor(){
+    super()
+    this.state= {}
+    this.onScroll = this.onScroll.bind(this) 
+}
+onScroll() {
+    var currentScroll = window.scrollTop / (contentScrollHeight);
+    
+  shadowTop.style.opacity = currentScroll;
+  shadowBottom.style.opacity = 1 - currentScroll;
+}
+render(){
+    const shadowScroll=()=>{
+var content = document.querySelector('.content'),
+    wrapper = document.querySelector('.wrapper'),
+    shadowTop = document.querySelector('.shadow--top'),
+    shadowBottom = document.querySelector('.shadow--bottom'),
+    contentScrollHeight = content.scrollHeight - wrapper.offsetHeight;
 
+content.addEventListener('scroll', this.onScroll);
 
-// render(){
-//     const shadowScroll=()=>{
-// var content = document.querySelector('.content'),
-//     wrapper = document.querySelector('.wrapper'),
-//     shadowTop = document.querySelector('.shadow--top'),
-//     shadowBottom = document.querySelector('.shadow--bottom'),
-//     contentScrollHeight = content.scrollHeight - wrapper.offsetHeight;
+    return (
+        <div class='wrapper'>
+            <div class='shadow shadow--top'></div> 
+            <div class='shadow shadow--bottom'></div> 
+            <div class='content'>
+            {}
+            </div>
+        </div>
+    )
+};
 
-// content.addEventListener('scroll', function(){
-//   var currentScroll = this.scrollTop / (contentScrollHeight);  
-//   shadowTop.style.opacity = currentScroll;
-//   shadowBottom.style.opacity = 1 - currentScroll;
-// });
-// }
-//     return (
-//         <div class='wrapper'>
-//             <div class='shadow shadow--top'></div> 
-//             <div class='shadow shadow--bottom'></div> 
-//             <div class='content'>
-//             {}
-//             </div>
-//         </div>
-//     )
-
-// };
-
-// }
-// export default Scroll;
+}
+}
+export default Scroll;
